7747e61fd35d1a0d61b767f2be96721e7f53e3ea
Jan Gevaert <jan.gevaert@gmail.com>
2021-06-17 16:49:55 -0600

    Initial commit

---
+++ src/edu/cnm/deepdive/CodingBatTheDoubleChar.java
@@ -0,0 +1,29 @@
+package edu.cnm.deepdive;
+
+/*
+Given a string, return a string where for every char in the original, there are two chars.
+
+doubleChar("The") → "TThhee"
+doubleChar("AAbb") → "AAAAbbbb"
+doubleChar("Hi-There") → "HHii--TThheerree"
+ */
+
+public class CodingBatTheDoubleChar {
+
+  public static void main(String[] args) {
+
+    System.out.printf("For the String %s, the result will be: " + doubleChar("The") + ".\n", "The");
+    System.out.printf("For the String %s, the result will be: " + doubleChar("AAbb") + ".\n", "AAbb");
+    System.out.printf("For the String %s, the result will be: " + doubleChar("Hi-There") + ".\n", "Hi-There");
+  }
+
+  public static String doubleChar(String str) {
+    String result = "";
+
+    for (int i = 0; i < str.length(); i++) {
+      result = result + str.charAt(i) + str.charAt(i);
+    }
+    return result;
+  }
+
+}
